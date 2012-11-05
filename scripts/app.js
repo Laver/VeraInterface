2ecad1e2958877406b08605ab8a4301a85b4384a
James Laver <jlaver@gmail.com>
2012-11-04 21:21:02 -0500

    Added models and controllers for Rooms and Devices, updated index to render list of rooms and devices

--- scripts/app.js
+++ scripts/app.js
@@ -28,6 +28,56 @@ App.Person = Em.Object.extend({
   }.property('firstName', 'lastName')
 });
 
+App.Room = Em.Object.extend ({
+    id: null,
+    name: null,
+    devices: []
+});
+ 
+ 
+App.Device = Em.Object.extend ({
+    
+    id: null,
+    name: null,
+    device_type: null,
+    room: null,
+    status: null,
+    states: [],
+    isOn: function () { return (this.status == "1") }.property("status"),
+    
+});
+
+App.Switch = App.Device.extend ({
+    
+    status: null,
+    setStates: function ( statesArray ) {
+        this.states = statesArray;
+        this.status = statesArray.findProperty("variable", "Status").value;
+    }
+    
+});
+App.DimmableLight = App.Device.extend ({
+    
+    status: null,
+    currentLevel: null,
+    targetLevel: null,
+    setStates: function ( statesArray ) {
+        this.states = statesArray;
+        this.status = statesArray.findProperty("variable", "Status").value;
+        this.currentLevel = statesArray.findProperty("variable", "LoadLevelStatus").value;
+        this.targetLevel = statesArray.findProperty("variable", "LoadLevelTarget").value;
+    }
+    
+});
+
+App.DeviceTypeEnum = {
+    
+    "urn:schemas-upnp-org:device:DimmableLight:1" : App.DimmableLight,
+    "urn:schemas-upnp-org:device:BinaryLight:1" : App.Switch
+    
+    
+};
+
 /**************************
 * Views
 **************************/
@@ -39,14 +89,30 @@ App.ApplicationView = Em.View.extend({
 * Controllers
 **************************/
 
-App.mainController = Em.ArrayController.create({
-    url:"data/lu_status2.json",
+App.mainController = Em.ObjectController.create({
+    statusUrl:"data/lu_status2.json",
+    dataUrl:"data/user_data2.json",
     status : "Ready",
-    veradata : [],
+    veradata : {},
+    verastatus : {},
+    loadStatus : function () {
+     
+        var me = this;
+        var url = me.get("statusUrl");
+        me.set("status", "Loading..." + url);
+        
+        $.getJSON(url, function(data){
+            me.set('verastatus', []);
+            $(data).each(function(index,value){
+                console.log(index, value);
+                //me.pushObject(t);
+            })
+        })
+    },
     loadData : function () {
      
         var me = this;
-        var url = me.get("url");
+        var url = me.get("dataUrl");
         me.set("status", "Loading..." + url);
         
         $.getJSON(url, function(data){
@@ -54,11 +120,66 @@ App.mainController = Em.ArrayController.create({
             $(data).each(function(index,value){
                 console.log(index, value);
                 //me.pushObject(t);
-            })
+            });
+            App.roomController.initRooms(data.rooms);
+            App.deviceController.initDevices(data.devices);
         });
     }
     
 });
 
+App.roomController = Em.ArrayController.create({
+    content : [],
+    initRooms : function ( roomArray ) {
+        var me = this;
+        // clear the rooms list
+        me.set("content",[]);
+        // loop through the passed array and create new room items
+        roomArray.forEach(function (item){
+            var newRoom = App.Room.create( { name : item.name , id : item.id } );
+            me.pushObject(newRoom);
+        });
+        
+        console.log("Rooms:", me.get("length"), me.content)
+    }
+    
+});
+
+App.deviceController = Em.ArrayController.create({
+    content : [],
+    initDevices : function ( deviceArray ) {
+        var me = this;
+        // clear the device list
+        me.set("content",[]);
+        // loop through the passed array and create new room items
+        deviceArray.forEach(function (item){
+            var newDevice = App.Device.create( { 
+                    name : item.name , 
+                    id : item.id, 
+                    room : item.room,
+                    device_type : item.device_type
+                });
+            
+            var deviceClass = App.DeviceTypeEnum[item.device_type];
+            
+            // if the device is in the enum, then we have a class to build, 
+            // otherwise we should skip it
+            if (deviceClass)
+            {
+                var testDevice = deviceClass.create( {
+                        name : item.name , 
+                        id : item.id, 
+                        room : item.room,
+                        device_type : item.device_type
+                    });
+                testDevice.setStates(item.states)
+                me.pushObject(testDevice);
+            }
+        });
+        
+        console.log("Devices:", me.get("length"), me.content)
+    }
+    
+});
 
 App.ApplicationController = App.mainController;
