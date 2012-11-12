8161958321968f9e4a261c6a741d12e5b29a8063
James Laver <jlaver@gmail.com>
2012-11-11 20:22:22 -0500

    Added handling of Scenes

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,15 @@ var App = Ember.Application.create({
             index: Ember.Route.extend({
                 route: '/'
             }),
+            scenes:  Ember.Route.extend({
+                route: '/scenes',
+                connectOutlets:  function(router, context){
+                  router.get('applicationController').connectOutlet('sceneList', App.Scenes.allSorted());
+                },
+                enter: function ( router ){
+                  //
+                }
+            }),
             rooms:  Ember.Route.extend({
                 route: '/rooms',
                 connectOutlets:  function(router, context){
@@ -27,6 +36,7 @@ var App = Ember.Application.create({
                 }
             }),
             
+            listScenes : Ember.Route.transitionTo("scenes"),
             listRooms : Ember.Route.transitionTo("rooms"),
             listDevices : Ember.Route.transitionTo("devices"),
             reloadData : function (router) {
@@ -36,12 +46,17 @@ var App = Ember.Application.create({
     }),
     
     /**************************
-    * Views
+    * Views & Controllers
     **************************/
     
     ApplicationView : Em.View.extend({
       templateName: 'application'
     }),
+    
+    SceneListView : Em.View.extend({
+      templateName: 'scene-list'
+    }),
+    SceneListController: Em.ArrayController.extend(),
     
     RoomListView : Em.View.extend({
       templateName: 'room-list'
@@ -78,8 +93,10 @@ var App = Ember.Application.create({
             me.set("status", "Loading..." + url);
             
             $.getJSON(url, function(data){
+                console.log(data);
                 App.Rooms.initRooms(data.rooms);
                 App.Devices.initDevices(data.devices);
+                App.Scenes.initScenes(data.scenes);
                 me.set("status", "Complete. Rooms:"+ App.Rooms.all().get("length") +" Devices:" + App.Devices.all().get("length") );
             });
         },
@@ -101,15 +118,11 @@ var App = Ember.Application.create({
 * Models
 **************************/
 
-App.Person = Em.Object.extend({
-  firstName: null,
-  lastName: null,
-  fullName: function() {
-    return this.get('firstName') +
-           " " + this.get('lastName');
-  }.property('firstName', 'lastName')
+App.Scene = Em.Object.extend ({
+    id: null,
+    name: null,
+    room: null
 });
-
 App.Room = Em.Object.extend ({
     id: null,
     name: null,
@@ -146,7 +159,6 @@ App.Device = Em.Object.extend ({
 
 App.Switch = App.Device.extend ({
     
-    status: null,
     setStates: function ( statesArray ) {
         this.states = statesArray;
         this.status = statesArray.findProperty("variable", "Status").value;
@@ -154,8 +166,7 @@ App.Switch = App.Device.extend ({
     
 });
 App.DimmableLight = App.Device.extend ({
-    
-    status: null,
+
     currentLevel: null,
     targetLevel: null,
     isAtTarget: function () { return (this.currentLevel == this.targetLevel); },
@@ -174,6 +185,50 @@ App.DeviceTypeEnum = {
     "urn:schemas-upnp-org:device:BinaryLight:1" : App.Switch
 
 };
+
+App.Scenes = Ember.Object.extend();
+App.Scenes.reopenClass({
+  _listOfScenes:  Em.A(),
+  _sortedScenes:  Em.A(),
+  all:  function(){
+    var allScenes = this._listOfScenes;
+    
+    // AJAX call would go here to populate as required
+    
+    return this._listOfScenes;
+  },
+  allSorted : function (){
+      return this._sortedScenes;
+  },
+  find:  function(id){
+    return this._listOfScenes.findProperty('id', id);
+  },
+  initScenes : function ( sceneArray ) {
+        console.log("initScenes");
+        var ds = this._listOfScenes;
+        ds.sortProperties = ['room'];
+        // clear the scene list
+        ds.clear();
+        // loop through the passed array and create new room items
+        sceneArray.forEach(function (item){
+            
+            // only add a scene if it has a "groups" property, as these should not be triggered scenes
+            if (item.groups){
+                var newScene = App.Scene.create( { name : item.name , id : item.id, room : item.room } );
+                ds.pushObject(newScene);
+            }
+        });
+        var sorted =  ds.sort(function(a,b) {
+            return a.get('room') - b.get('room');
+        });
+        var sc = this._sortedScenes;
+        sorted.forEach (function (item) {
+            sc.pushObject(item);
+        });
+        
+    }
+});
+
 
 App.Rooms = Ember.Object.extend();
 App.Rooms.reopenClass({
@@ -200,6 +255,8 @@ App.Rooms.reopenClass({
         });
     }
 });
+
+
 App.Devices = Ember.Object.extend();
 App.Devices.reopenClass({
   _listOfDevices:  Em.A(),
@@ -242,13 +299,6 @@ App.Devices.reopenClass({
     }
 });
 
-/**************************
-* Controllers
-**************************/
-
-
 
 
 App.initialize();
-
-
